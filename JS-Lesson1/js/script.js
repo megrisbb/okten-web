7a7b96a7169d0b2572dbe4d16cbda73d6b9e2304
megrisbb <megrisbbmother@gmail.com>
2022-11-01 14:07:01 +0200

    JS Lesson 1 HW

---
+++ JS-Lesson1/js/script.js
@@ -0,0 +1,50 @@
+const hello = 'hello';
+console.log(hello)
+const okten = 'owu';
+console.log(okten)
+const domain = 'com';
+console.log(domain)
+const ukraine = 'ua';
+console.log(ukraine)
+const first = 1;
+console.log(first)
+const ten = 10;
+console.log(ten)
+const minus = -999;
+console.log(minus)
+const threeNumbers = 123;
+console.log(threeNumbers)
+const PI = 3.14;
+console.log(PI)
+const numb = 2.7;
+console.log(numb)
+const lastNubmer = 16;
+console.log(lastNubmer)
+const firstT = true;
+console.log(firstT)
+const secondF = false;
+console.log(secondF)
+
+let firstName = 'Bohdan';
+let middleName = 'Myronovich';
+let lastName = 'Baryliak';
+
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person)
+
+let a = 100;
+console.log(typeof a)
+let b = '100';
+console.log(typeof b)
+let c = true;
+console.log(typeof c)
+
+myName = prompt('Bohdan');
+console.log(myName)
+myMiddleName = prompt('Myronovich');
+console.log(myMiddleName)
+myAge = prompt(25);
+console.log(myAge)
+
+
+
